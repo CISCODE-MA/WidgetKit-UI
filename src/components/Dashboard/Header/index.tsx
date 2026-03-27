f6a554d549440365e141b22920f9cb1e39c261eb
a-elkhiraooui-ciscode <a.elkhiraoui@ciscod.com>
2026-03-27 17:07:54 +0100

    fixed layering

--- src/components/Dashboard/Header/index.tsx
+++ src/components/Dashboard/Header/index.tsx
@@ -16,7 +16,10 @@ const Header = (props: {
   console.log('HEADER onLogout prop =', props.onLogout);
 
   return (
-    <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
+    <header
+      className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none"
+      style={{ zIndex: 10001 }}
+    >
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
         <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
           {/* <!-- Hamburger Toggle BTN --> */}
